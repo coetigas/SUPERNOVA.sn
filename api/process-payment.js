06f7b65213b61cb6ddc712a77f4900934f66f2b7
coetigas <tiagoluciano985@gmail.com>
2026-07-28 15:26:22 -0300

    Update process-payment.js

--- api/process-payment.js
+++ api/process-payment.js
@@ -18,6 +18,7 @@ export default async function handler(req, res) {
 
   try {
     const body = req.body;
+    console.log("BODY RECEBIDO:", JSON.stringify(body));
     const {
       token,
       issuer_id,
